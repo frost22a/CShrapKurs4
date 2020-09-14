e3b66c5d9e38620e114a711d832ab5cd3b71b7d4
Przemek Mroz <frost22a@gmail.com>
2020-09-14 09:35:58 +0200

    arrays lecture plus examples

--- CSharpCollections/Arrays/Program.cs
+++ CSharpCollections/Arrays/Program.cs
@@ -6,7 +6,58 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // inicjalizacja najprostszej tablicy, gdy z góry znamy jakie rekordy będą w niej zawarte 
+            string[] monthOfYear =
+            {
+                "January",
+                "february",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "Optober",
+                "November",
+                "December"
+            };
+
+            #region array examples
+            int[] arrayInt = {4, 77 ,24,36 };
+            char[] arrayChar = { 'g', 'l', 'a', 's' };
+            System.Drawing.Rectangle[] arrayRectangle =
+                {
+                    new System.Drawing.Rectangle(10,10,50,50),
+                    new System.Drawing.Rectangle(20,30,440,60),
+                    new System.Drawing.Rectangle(10,40,150,250)
+                };
+            #endregion
+
+            #region foreach in arrays
+            //foreach pozwala przejść przez każdy element kolekcji
+            foreach (var month2 in monthOfYear)
+            {
+                Console.WriteLine(month2);
+            }
+            #endregion
+
+            #region display months using array
+            Console.WriteLine("Which month do You Want to display (1 - January, 2 - february, etc.)");
+            int month = int.Parse(Console.ReadLine());
+            string chosenMonth = monthOfYear[month-1];
+
+
+            Console.WriteLine($"You choose: {monthOfYear}");
+            #endregion
+
+            #region editing arrays
+
+            monthOfYear[1] = "February";
+
+            #endregion
+
         }
     }
 }
