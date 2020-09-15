bdbeaaf0897c837dd19b8589e220c65708c94c58
Przemek Mroz <frost22a@gmail.com>
2020-09-15 23:37:22 +0200

    Working with lists - very good examples - worth to remember plus "predicate" used in removeall command (two ways - one with lambda expression, second - a static bool method)

---
+++ CSharpCollections/WorkWithLists/CsvReader.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace WorkWithLists
+{
+    class CsvReader
+    {
+        private string csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this.csvFilePath = csvFilePath;
+        }
+
+        public List<City> ReadAllCities() 
+        {
+            List<City> cities = new List<City>();
+
+            using (StreamReader streamReader = new StreamReader(csvFilePath)) // użycie using powoduje zamknięcie StreamReadera po zakończeniu czytania pliku, 
+                                                                              // jeżeli zostałby nadal otwarty , to inne progarmy nie miałyby dostepu do pliku population.csv
+            {
+                streamReader.ReadLine();
+                
+                string csvLine;
+                while ((csvLine = streamReader.ReadLine()) != null)
+                {
+                    cities.Add(ReadCityFromCsvFile(csvLine));
+                }
+                
+            }
+                return cities;
+        }
+
+        public City ReadCityFromCsvFile(string csvLine)
+        {
+            // tworzymy tablicę stringów, dla której separatorem jest ','
+            string[] parts = csvLine.Split(new char[] { ',' }); // tu zamiast char[] można wpisać inne typy, a w nawiasach wypisać separatory. mozna też zostawić .Spilt(',')
+
+            string cityName = parts[0];
+            string cityCode = parts[1];
+            string country = parts[2];
+            int totalPopulation = int.Parse(parts[3]);
+            int menPopulation = int.Parse(parts[4]);
+            int womenPopulation = int.Parse(parts[5]);
+
+            return new City(cityName, cityCode, country, totalPopulation, menPopulation, womenPopulation);        
+        }
+
+        public void RemoveCityTest(List<City> cities)
+        {
+            for (int i = 0; i < cities.Count; i++)
+            {
+                if (cities[i].CityName.Contains("Test"))
+                    cities.RemoveAt(i);
+                    i--; //-> dobre rozwiązanie, ale lepiej zastosować listowanie od góry do dołu:
+            }
+        }
+
+        public void RemoveCityTest2(List<City> cities)
+        {
+            for (int i = cities.Count - 1; i >= 0; i--)
+            {
+                if (cities[i].CityName.Contains("Test"))
+                    cities.RemoveAt(i);
+                // i--; -> dobre rozwiązanie, ale lepiej zastosować listowanie od góry do dołu:
+            }
+        }
+
+        // zamiast korzystać z pętli for można użyć RemoveAll():
+
+
+        public void RemoveCityTest3(List<City> cities)
+        {
+            cities.RemoveAll(c => c.CityName.Contains("Test")); //predykat!!! za pomocą lambdy
+        }
+        public void RemoveCityTest4(List<City> cities)
+        {
+            cities.RemoveAll(predykat); // predykat wpisany za pomocą metody zwracającej bool 
+        }
+
+        public static bool predykat(City city) 
+        {
+            return city.CityName.Contains("Test");
+        }
+
+    }
+}
---
+++ CSharpCollections/WorkWithLists/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkWithLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string filePath = @"D:\PROGRAMOWANIE\C#\CShrapKurs4\CSharpCollections\WorkWithLists\population.csv"; //@ -> "czytanie dosłowne"
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            List<City> cities = csvReader.ReadAllCities();
+
+            csvReader.RemoveCityTest(cities);
+
+            //foreach (var city in cities)
+            //{
+            //    Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            //}
+
+            //wersja z pętlą FOR
+
+            //for (int i = 0; i < cities.Count; i++)
+            //{
+            //    var city = cities[i];
+            //    Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            //}
+
+            //wyświetlanie określonej ilości miast:
+
+            //Console.Write("Enter, how many cities you want to diplay: ");
+            //bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+
+            //if (!inputIsInt || userInput <= 0)
+            //{
+            //    Console.WriteLine("You must enter a positve integer!");
+            //    return;
+            //}
+
+            //int maxToDisplay = Math.Min(userInput, cities.Count);
+
+            //for (int i = 0; i < maxToDisplay; i++)
+            //{
+            //    var city = cities[i];
+            //    Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            //}
+            
+            //wyświetlanie określonej liczyby miast w kolejnych krokach 
+            
+            Console.Write("Enter, how many cities you want to diplay: ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+
+            if (!inputIsInt || userInput <= 0)
+            {
+                Console.WriteLine("You must enter a positve integer!");
+                return;
+            }
+
+            int maxToDisplay = Math.Min(userInput, cities.Count);
+
+            for (int i = 0; i < cities.Count; i++)
+            {
+                if (i > 0 && (i % userInput == 0))
+                {
+                    Console.WriteLine("Hit enter to continue, to quit press anything else");
+                    if (Console.ReadLine() != "")
+                    {
+                        break;
+                    }
+
+                }
+                var city = cities[i];
+                Console.WriteLine($"{i + 1} - {city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            }
+
+            // aby liczyć od końca musimy zmienić warunek pętli:
+
+            //for (int i = cities.Count-1; i >= 0; i--)
+            //{
+            //    int displayIndex = cities.Count - 1 - i;
+            //    if (displayIndex > 0 && (displayIndex % userInput == 0))
+            //    {
+            //        Console.WriteLine("Hit enter to continue, to quit press anything else");
+            //        if (Console.ReadLine() != "")
+            //        {
+            //            break;
+            //        }
+
+            //    }
+            //    var city = cities[i];
+            //    Console.WriteLine($"{displayIndex + 1} - {city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            //}
+        }
+    }
+}
