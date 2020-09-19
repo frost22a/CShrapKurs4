c05daac63ac36c78fc3430659351a50d372f3357
Przemek Mroz <frost22a@gmail.com>
2020-09-19 13:23:56 +0200

    few good examples, need to remember

---
+++ CSharpCollections/CollectionsOfCollections/CsvReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CollectionsOfCollections
+{
+    class CsvReader
+    {
+        private string csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this.csvFilePath = csvFilePath;
+        }
+
+        public Dictionary <string, List<City>> ReadAllCities() 
+        {
+            Dictionary<string, List<City>> cities = new Dictionary<string, List<City>>();
+
+            using (StreamReader streamReader = new StreamReader(csvFilePath)) // użycie using powoduje zamknięcie StreamReadera po zakończeniu czytania pliku, 
+                                                                              // jeżeli zostałby nadal otwarty , to inne progarmy nie miałyby dostepu do pliku population.csv
+            {
+                streamReader.ReadLine();
+                
+                string csvLine;
+                while ((csvLine = streamReader.ReadLine()) != null)
+                {
+                    City city = ReadCityFromCsvFile(csvLine);
+                    
+                    if (cities.ContainsKey(city.Country))
+                    {
+                        cities[city.Country].Add(city);
+                    }
+                    else
+                    {
+                        List<City> cityInCounrty = new List<City>() { city };
+                        cities.Add(city.Country, cityInCounrty);
+                    }
+                }
+                
+            }
+                return cities;
+        }
+
+        public City ReadCityFromCsvFile(string csvLine)
+        {
+            // tworzymy tablicę stringów, dla której separatorem jest ','
+            string[] parts = csvLine.Split(new char[] { ',' }); // tu zamiast char[] można wpisać inne typy, a w nawiasach wypisać separatory. mozna też zostawić .Spilt(',')
+
+            string cityName = parts[0];
+            string cityCode = parts[1];
+            string country = parts[2];
+            int totalPopulation = int.Parse(parts[3]);
+            int menPopulation = int.Parse(parts[4]);
+            int womenPopulation = int.Parse(parts[5]);
+
+            return new City(cityName, cityCode, country, totalPopulation, menPopulation, womenPopulation);        
+        }
+    }
+}
---
+++ CSharpCollections/CollectionsOfCollections/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionsOfCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string filePath = @"D:\PROGRAMOWANIE\C#\CShrapKurs4\CSharpCollections\CollectionsOfCollections\population.csv"; //@ -> "czytanie dosłowne"
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            Dictionary<string, List<City>> cities = new Dictionary<string, List<City>>();
+
+            cities = csvReader.ReadAllCities();
+
+            //var city = cities["Polska"].ElementAt(0);
+            //Console.WriteLine(cities["Polska"].ElementAt(2).CityName);
+            //Console.WriteLine(city.CityName);
+
+
+            //foreach (KeyValuePair<string, List<City>> pair in cities)
+            //{
+            //    Console.WriteLine(pair);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("cities.Keys");
+
+            foreach (string countryName in cities.Keys) // string można zastąpić przez var
+            {
+                Console.WriteLine(countryName);
+            }
+
+            //Console.WriteLine();
+            //Console.WriteLine("cities.Values");
+
+            //foreach (List<City> List in cities.Values) // List<City> można zastąpić przez var
+            //{
+            //    Console.WriteLine(List);
+            //}
+
+            
+
+            while (true)
+            {
+                
+                Console.WriteLine("Choose a counrty (leave blank to quit):");
+                var choosenCountry = Console.ReadLine();
+
+                if (choosenCountry == "")
+                {
+                    break;
+                }
+                
+                if (cities.ContainsKey(choosenCountry))
+                {
+                    foreach (City city in cities[choosenCountry])
+                        Console.WriteLine($"{city.TotalPopulation: ### ### ###} : {city.CityName}");
+                }
+                else
+                    Console.WriteLine("There is no such a counrty on the list");
+            }
+
+            
+
+        }
+    }
+}
